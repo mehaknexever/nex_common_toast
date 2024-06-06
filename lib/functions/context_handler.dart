7b49b1e3240b0d9243902a3cfba93df1c975e23d
Harvinder Singh <harvinder.nexever@gmail.com>
2024-06-06 17:48:13 +0530

    documentation added

--- lib/functions/context_handler.dart
+++ lib/functions/context_handler.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
 
+/// A class for managing the application context.
+///
+/// The `ContextManager` class provides a way to set and retrieve the current `BuildContext`
+/// within the application. It follows the singleton design pattern to ensure there's only
+/// one instance of the context manager throughout the application.
+///
+/// Example usage:
+/// ```dart
+/// // Instantiate the ContextManager
+/// ContextManager contextManager = ContextManager();
+///
+/// // Set the BuildContext
+/// contextManager.setContext(context);
+///
+/// // Retrieve the BuildContext
+/// BuildContext? currentContext = contextManager.context;
+/// ```
 class ContextManager {
-  // Private static instance
+  /// Private static instance
   static final ContextManager _instance = ContextManager._internal();
 
-  // Private constructor
+  /// Private constructor
   ContextManager._internal();
 
-  // Factory constructor
+  /// Factory constructor
   factory ContextManager() {
     return _instance;
   }
 
-  // Variable to hold BuildContext
+  /// Variable to hold BuildContext
   BuildContext? _context;
 
-  // Method to set context
+  /// Sets the current BuildContext.
+  ///
+  /// Parameters:
+  /// - [context] (`BuildContext`): The BuildContext to set.
   void setContext(BuildContext context) {
     _context = context;
   }
 
-  // Method to get context
+  /// Gets the current BuildContext.
+  ///
+  /// Returns:
+  /// The current BuildContext, or `null` if not set.
   BuildContext? get context => _context;
 }
--- lib/functions/toast_file.dart
+++ lib/functions/toast_file.dart
@@ -2,7 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:oktoast/oktoast.dart' as toast;
 import 'package:oktoast/oktoast.dart';
 
+/// An abstract class that provides a method to display toast messages.
+///
+/// The `ToastFile` class defines a method for showing toast notifications with customizable
+/// parameters such as message text, background color, position, duration, text style, and corner radius.
+///
+/// Example usage:
+/// ```dart
+/// class MyToast extends ToastFile {
+///   @override
+///   void toastMsg({
+///     required String msg,
+///     Color? backgroundColor,
+///     ToastPosition? position,
+///     Duration? duration,
+///     TextStyle? textStyle,
+///     double? radius,
+///   }) {
+///     super.toastMsg(
+///       msg: msg,
+///       backgroundColor: backgroundColor,
+///       position: position,
+///       duration: duration,
+///       textStyle: textStyle,
+///       radius: radius,
+///     );
+///   }
+/// }
+/// ```
 abstract class ToastFile {
+  /// Displays a toast message with the specified parameters.
+  ///
+  /// Parameters:
+  /// - [msg] (`String`): The message to display in the toast. This parameter is required.
+  /// - [backgroundColor] (`Color?`): The background color of the toast. Defaults to a black color with 80% opacity if not provided.
+  /// - [position] (`ToastPosition?`): The position where the toast will be displayed. Defaults to `ToastPosition.center` if not provided.
+  /// - [duration] (`Duration?`): The duration for which the toast will be displayed. Defaults to 5 seconds if not provided.
+  /// - [textStyle] (`TextStyle?`): The style to use for the toast message text. Defaults to a white, 12-point font if not provided.
+  /// - [radius] (`double?`): The radius of the corners of the toast. Defaults to 13.0 if not provided.
+  ///
+  /// Example:
+  /// ```dart
+  /// toastMsg(
+  ///   msg: "This is a toast message.",
+  ///   backgroundColor: Colors.red,
+  ///   position: ToastPosition.bottom,
+  ///   duration: Duration(seconds: 3),
+  ///   textStyle: TextStyle(fontSize: 16.0, color: Colors.yellow),
+  ///   radius: 10.0,
+  /// );
+  /// ```
   void toastMsg({
     required String msg,
     Color? backgroundColor,
