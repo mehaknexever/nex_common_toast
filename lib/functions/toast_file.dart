f28af3761017697b7e1dc1b30abde0380cd27f10
Harvinder Singh <harvinder.nexever@gmail.com>
2024-06-10 12:16:38 +0530

    toast file updated changes

--- lib/functions/toast_file.dart
+++ lib/functions/toast_file.dart
@@ -31,34 +31,67 @@ import 'package:oktoast/oktoast.dart';
 /// }
 /// ```
 abstract class ToastFile {
-  /// Displays a toast message with the specified parameters.
+  /// Displays a customizable toast message.
   ///
-  /// Parameters:
-  /// - [msg] (`String`): The message to display in the toast. This parameter is required.
-  /// - [backgroundColor] (`Color?`): The background color of the toast. Defaults to a black color with 80% opacity if not provided.
-  /// - [position] (`ToastPosition?`): The position where the toast will be displayed. Defaults to `ToastPosition.center` if not provided.
-  /// - [duration] (`Duration?`): The duration for which the toast will be displayed. Defaults to 5 seconds if not provided.
-  /// - [textStyle] (`TextStyle?`): The style to use for the toast message text. Defaults to a white, 12-point font if not provided.
-  /// - [radius] (`double?`): The radius of the corners of the toast. Defaults to 13.0 if not provided.
+  /// The `toastMsg` function allows you to show a toast message with various
+  /// customizable options such as message text, background color, position,
+  /// duration, text style, border radius, text overflow handling, maximum text
+  /// lines, text alignment, and text direction.
   ///
-  /// Example:
+  /// The function uses default values for the optional parameters if they are
+  /// not provided.
+  ///
+  /// Example usage:
   /// ```dart
   /// toastMsg(
-  ///   msg: "This is a toast message.",
-  ///   backgroundColor: Colors.red,
+  ///   msg: "Hello, World!",
+  ///   backgroundColor: Colors.blue,
   ///   position: ToastPosition.bottom,
   ///   duration: Duration(seconds: 3),
-  ///   textStyle: TextStyle(fontSize: 16.0, color: Colors.yellow),
+  ///   textStyle: TextStyle(fontSize: 16.0, color: Colors.white),
   ///   radius: 10.0,
+  ///   textOverflow: TextOverflow.ellipsis,
+  ///   textMaxLines: 2,
+  ///   textAlign: TextAlign.left,
+  ///   textDirection: TextDirection.rtl,
   /// );
   /// ```
-  void toastMsg({
+  ///
+  /// Parameters:
+  /// - `msg` (String): The message to be displayed in the toast.
+  /// - `backgroundColor` (Color?, optional): The background color of the toast.
+  ///   Defaults to `Colors.black.withOpacity(0.8)`.
+  /// - `position` (ToastPosition?, optional): The position where the toast
+  ///   should be displayed. Defaults to `ToastPosition.center`.
+  /// - `duration` (Duration?, optional): The duration for which the toast
+  ///   should be displayed. Defaults to `Duration(seconds: 5)`.
+  /// - `textStyle` (TextStyle?, optional): The text style of the message.
+  ///   Defaults to `TextStyle(fontSize: 12.0, color: Colors.white)`.
+  /// - `radius` (double?, optional): The border radius of the toast.
+  ///   Defaults to `13.0`.
+  /// - `textOverflow` (TextOverflow?, optional): How the text should overflow.
+  ///   Defaults to `TextOverflow.ellipsis`.
+  /// - `textMaxLines` (int?, optional): The maximum number of lines for the
+  ///   text. Defaults to `8`.
+  /// - `textAlign` (TextAlign?, optional): How the text should be aligned.
+  ///   Defaults to `TextAlign.center`.
+  /// - `textDirection` (TextDirection?, optional): The text direction for the
+  ///   message. Defaults to `TextDirection.ltr`.
+  ///
+  /// Note:
+  /// Ensure the toast package is properly initialized and imported before
+  /// using this function.
+  static void toastMsg({
     required String msg,
     Color? backgroundColor,
     ToastPosition? position,
     Duration? duration,
     TextStyle? textStyle,
     double? radius,
+    TextOverflow? textOverflow,
+    int? textMaxLines,
+    TextAlign? textAlign,
+    TextDirection? textDirection,
   }) {
     toast.showToast(
       msg,
@@ -69,6 +102,10 @@ abstract class ToastFile {
       textStyle:
           textStyle ?? const TextStyle(fontSize: 12.0, color: Colors.white),
       dismissOtherToast: true,
+      textOverflow: textOverflow ?? TextOverflow.ellipsis,
+      textMaxLines: textMaxLines ?? 8,
+      textAlign: textAlign ?? TextAlign.center,
+      textDirection: textDirection ?? TextDirection.ltr,
     );
   }
 }
